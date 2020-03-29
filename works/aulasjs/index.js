65f7450a7f97f8e33a842e810e9ff9af1eedd62d
Bruno Mosconi <contato.brunomosconi@gmail.com>
2020-03-29 15:05:34 -0300

    update index

--- works/aulasjs/index.js
+++ works/aulasjs/index.js
@@ -1,8 +1,8 @@
 // Criar um programa que calcula a média
-// das notas entre os alunos e envia
+// das turmas de alunos e envia
 // mensagem do cálculo da média
 
-const alunos = [
+const alunosDaTurmaA = [
     {
         nome: "Bruno",
         nota: 9.8
@@ -14,41 +14,62 @@ const alunos = [
     {
         nome: "Noah",
         nota: 5
+    },
+    {
+        nome: "Novo Aluno",
+        nota: 7
     }
 ]
 
-const aluno01 = {
-    nome: "Bruno",
-    nota: 9.8
+const alunosDaTurmaB = [
+    {
+        nome: "Pedro",
+        nota: 7
+    },
+    {
+        nome: "João",
+        nota: 8
+    },
+    {
+        nome: "Tiago",
+        nota: 7.5
+    }
+]
+
+
+
+function calculaMediaFixa(alunos) {
+    return (alunos[0].nota + alunos[1].nota + 
+        alunos[2].nota) / 3
 }
 
-
-const aluno02 = {
-    nome: "Josh",
-    nota: 10
+function calculaMedia(alunos) {
+    let soma = 0;
+    for (let i = 0; i < alunos.length; i++) {
+        soma = soma + alunos[i].nota
+    }
+    const media = soma / alunos.length;
+    return media
 }
 
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
 
-const aluno03 = {
-    nome: "Noah",
-    nota: 5
+function enviaMensagem(media, turma) {
+    // Se a média for maior que 5, parabenizar a turma
+
+    if (media > 5) {
+        console.log(`A media da turma ${turma} foi de ${media}, Parabéns`)
+    } else { 
+        console.log(`A média da turma ${turma} é menor que 5`)
+    }
+
 }
 
+enviaMensagem(media1, 'A')
+enviaMensagem(media2, 'B')
 
-const media2 = (alunos[0].nota + alunos[1].nota + 
-    alunos[2].nota) / 3
-
-
-const media = (aluno01.nota + aluno02.nota + 
-    aluno03.nota) / 3
+// console.log()
 
 
 
-
-// Se a média for maior que 5, parabenizar a turma
-
-if (media > 5) {
-    console.log(`A media foi de ${media}, Parabéns`)
-} else { 
-    console.log('A média é menor que 5')
-}
